90b41cb11b620e05774f7e67a6c01ef7325a2278
Detravoir <thomasdiondehaas@hotmail.com>
2023-03-31 09:51:26 +0200

    put everything in one page

--- register.php
+++ register.php
@@ -77,3 +77,76 @@ if (isset($_POST['submit'])) {
 </form>
 </body>
 </html> -->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <link href="src/css/reset.css" rel="stylesheet" />
+        <link href="src/css/styles.css" rel="stylesheet" />
+        <link href="src/css/footer.css" rel="stylesheet" />
+
+          <!-- font awesome icons -->
+        <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js"
+        integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe"
+        crossorigin="anonymous"></script>
+
+        <title>Medialab - planner</title>
+    </head>
+    <body>
+        <nav class="nav">
+            <img class="logo" src="src/images/logo.png" />
+            <div class="btn-div">
+                <a class="button" href="/"> Logout </a>
+            </div>
+        </nav>
+        <div class="big-container">
+            <div class="line">
+            </div>
+            <div class="main">
+                <div class="content">
+                    <div class="titel">
+                        Maak een account aan
+                    </div>
+                    <div class="border">
+
+                    </div>
+                    <form class="form" action="<?= $_SERVER['PHP_SELF']; ?>" method="post">
+                        <div class="email-content">
+                            <label class="label" for="email">E-mail:</label>
+                            <input class="input" type="text" id="email" name="email">
+                            <span class="errors"><?= isset($errors['email']) ? $errors['email'] : '' ?></span>
+                        </div>
+                        <div class="password-content">
+                            <label class="label" for="password">Password:</label>
+                            <input class="input" type="password" id="password" name="password">
+                            <span class="errors"><?= isset($errors['password']) ? $errors['password'] : '' ?></span>
+                        </div>
+                        <div class="name-content">
+                            <label class="label" for="name">Naam:</label>
+                            <input class="input" type="text" id="name" name="name">
+                            <span class="errors"><?= isset($errors['name']) ? $errors['name'] : '' ?></span>
+                        </div>
+                        <input class="button" type="submit" value="Aanmaken"> 
+                    </form>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <div class="rounded-social-buttons">
+                <a class="social-button facebook" href="https://www.facebook.com/" target="_blank"><i
+                        class="fab fa-facebook-f"></i></a>
+                <a class="social-button twitter" href="https://www.twitter.com/" target="_blank"><i
+                        class="fab fa-twitter"></i></a>
+                <a class="social-button linkedin" href="https://www.linkedin.com/" target="_blank"><i
+                        class="fab fa-linkedin"></i></a>
+                <a class="social-button youtube" href="https://www.youtube.com/" target="_blank"><i
+                        class="fab fa-youtube"></i></a>
+                <a class="social-button instagram" href="https://www.instagram.com/" target="_blank"><i
+                        class="fab fa-instagram"></i></a>
+            </div>
+        </footer>
+    </body>
+</html>
